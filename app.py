90cd4817b53d1be2729addd6716757835d8d46d4
Xavier Bonnin <xavierbonnin@gmail.com>
2024-02-12 16:54:55 +0100

    app insight logging

--- app.py
+++ app.py
@@ -1,4 +1,11 @@
 import os
+import logging
+from opencensus.ext.azure.log_exporter import AzureLogHandler
+from opencensus.ext.azure.trace_exporter import AzureExporter
+from opencensus.ext.flask.flask_middleware import FlaskMiddleware
+from opencensus.trace.samplers import ProbabilitySampler
+
+logger = logging.getLogger(__name__)
 
 from flask import (Flask, redirect, render_template, request,
                    send_from_directory, url_for)
@@ -9,6 +16,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
    print('Request for index page received')
+   logger.info('Request for index page received')
    return render_template('index.html')
 
 @app.route('/favicon.ico')
@@ -18,20 +26,23 @@ def favicon():
 
 @app.route('/hello', methods=['POST'])
 def hello():
+   logger.info('Get name popup')
    name = request.form.get('name')
 
    if name:
        print('Request for hello page received with name=%s' % name)
+       logger.info(f"Request for hello page received with name= {name}")  
        return render_template('hello.html', name = name)
    else:
        print('Request for hello page received with no name or blank name -- redirecting')
+       logger.warning('Request for hello page received with no name or blank name -- redirecting')
        return redirect(url_for('index'))
 
 @app.route('/healthcheck', methods=['GET'])
 def healthcheck():
        print('Healthcheck request')
+       logger.info('Healthcheck request')
        return render_template('hello.html', name = 'healthcheck')
-
 
 if __name__ == '__main__':
    app.run()
